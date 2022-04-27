60ec7d1945c3d3d33d49cf0710d873e169221b5f
shamique202 <shamique.e@gmail.com>
2022-04-27 17:44:41 -0400

    added a toy collection forms/info

--- main_app/models.py
+++ main_app/models.py
@@ -1,26 +1,40 @@
 from django.db import models
 # import the reverse function
 from django.urls import reverse
-
-class Puppy(models.Model):
-  name = models.CharField(max_length=100)
-  breed = models.CharField(max_length=100)
-  description = models.TextField(max_length=250)
-  age = models.IntegerField()
-
-  def __str__(self):
-    return self.name
-
-    # Add this method
-  def get_absolute_url(self):
-    return reverse('detail', kwargs={'puppy_id': self.id})
-
+from datetime import date
 # A tuple of 2-tuples
 MEALS = (
     ('B', 'Breakfast'),
     ('L', 'Lunch'),
     ('D', 'Dinner')
 )
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  # Add this method
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+class Puppy(models.Model):
+  name = models.CharField(max_length=100)
+  breed = models.CharField(max_length=100)
+  description = models.TextField(max_length=250)
+  age = models.IntegerField()
+  toys = models.ManyToManyField(Toy)
+
+def __str__(self):
+    return self.name
+
+def get_absolute_url(self):
+    return reverse('detail', kwargs={'puppy_id': self.id})
+
+def fed_for_today(self):
+    return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
 # Add new Feeding model below Puppy model
 class Feeding(models.Model):
@@ -40,9 +54,6 @@ class Feeding(models.Model):
   # change the default sort
   class Meta:
     ordering = ['-date']
-
-    
-
 	 
 
 	  
