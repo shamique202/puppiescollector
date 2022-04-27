84dd3f0f7cf7328c31c4ae9134ef7ef368f551dd
shamique202 <shamique.e@gmail.com>
2022-04-27 19:27:59 -0400

    adding comments througout

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 # add Feeding to the import
-from .models import Puppy, Feeding
+from .models import Puppy, Feeding, Toy
 #import models here:
 # Register your models here:
 admin.site.register(Puppy)
 admin.site.register(Feeding)
+admin.site.register(Toy)
--- main_app/views.py
+++ main_app/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render, redirect #always redirect after making chan
 # import a create view 
 # import UpdateView, DeleteView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 # Add the following import
-from .models import Puppy
+from .models import Puppy, Toy 
 from .forms import FeedingForm
 
 # Add the Puppy class & list and view function below the imports
@@ -12,12 +13,12 @@ from .forms import FeedingForm
 # the template the CreateView and the UpdateView use is the same
 class PuppyCreate(CreateView):
     model = Puppy
-    fields = '__all__'
-    success_url = '/puppies/'
+    fields = ['name', 'breed', 'description', 'age']
+    success_url = '/puppies/' # get_absolute_url doesnt work for create/update , so i can create a success_url and itll work
 class PuppyUpdate(UpdateView):
   model = Puppy
-  # Let's disallow the renaming of a Puppy by excluding the name field!
   fields = ['breed', 'description', 'age']
+  success_url = '/puppies/'
 
 class PuppyDelete(DeleteView):
   model = Puppy
@@ -34,22 +35,50 @@ def puppies_index(request):
   return render(request, 'puppies/index.html', { 'puppies': puppies })
 def puppies_detail(request, puppy_id):
   puppy = Puppy.objects.get(id=puppy_id)
+  toys_puppy_doesnt_have = Toy.objects.exclude(id__in = puppy.toys.all().values_list('id'))
+
   # instantiate FeedingForm to be rendered in the template
   feeding_form = FeedingForm()
   return render(request, 'puppies/detail.html', {
     # include the puppy and feeding_form in the context
-    'puppy': puppy, 'feeding_form': feeding_form
+    'puppy': puppy, 'feeding_form': feeding_form,
+    'toys': toys_puppy_doesnt_have
   })
+
 def add_feeding(request, puppy_id):
-  pass
 	# create the ModelForm using the data in request.POST
   form = FeedingForm(request.POST)
   # validate the form
   if form.is_valid():
     # don't save the form to the db until it
-    # has the cat_id assigned
+    # has the puppy_id assigned
     new_feeding = form.save(commit=False)
     new_feeding.puppy_id = puppy_id
     new_feeding.save()
   return redirect('detail', puppy_id=puppy_id)
   
+def assoc_toy(request, puppy_id, toy_id):
+  Puppy.objects.get(id=puppy_id).toys.add(toy_id)
+  return redirect('detail', puppy_id=puppy_id)
+
+def unassoc_toy(request, puppy_id, toy_id):
+  Puppy.objects.get(id=puppy_id).toys.remove(toy_id)
+  return redirect('detail', puppy_id=puppy_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
